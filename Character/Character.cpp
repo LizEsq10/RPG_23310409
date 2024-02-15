f99346b493ef14619365fb4757f701795d08457d
vmnavarro94 <vm.navarro94@gmail.com>
2024-02-15 09:20:56 -0600

    Create Character class

--- Character/Character.cpp
+++ Character/Character.cpp
@@ -3,13 +3,12 @@
 //
 
 #include "Character.h"
-Character::Character(string _name, int _health, int _attack, int _defense, int _speed, bool _isPlayer) {
+Character::Character(string _name, int _health, int _attack, int _defense, int _speed) {
     name = _name;
     health = _health;
     attack = _attack;
     defense = _defense;
     speed = _speed;
-    isPlayer = _isPlayer;
 }
 
 string Character::getName() {
@@ -34,16 +33,4 @@ int Character::getSpeed() {
 
 string Character::toString() {
     return "Name: " + name + "\nHealth: " + to_string(health) + "\nAttack: " + to_string(attack) + "\nDefense: " + to_string(defense) + "\nSpeed: " + to_string(speed);
-}
-
-bool Character::getIsPlayer() {
-    return isPlayer;
-}
-
-bool Character::flee(Character*target) {
-    if(this->speed > target->speed)
-        return true;
-
-    int chance = rand() % 100;
-    return chance > 30;
 }
